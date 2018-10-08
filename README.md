aec7ce53f31088c2908a56662c2091de19f1a8fb
mapsam <matthews.sam@gmail.com>
2018-10-08 13:22:05 -0700

    es6, node>=6, mapbox namespace

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
 [![Build Status](https://travis-ci.org/mapbox/mapbox-error.svg)](https://travis-ci.org/mapbox/mapbox-error)
 
-### Generic error middleware for express apps
+### Generic error middleware for express.js apps
 
 #### usage:
 ``` javascript
-var errors = require('mapbox-error');
-var ErrorHTTP = errors.ErrorHTTP;
-var server = require('express')();
+const errors = require('@mapbox/mapbox-error');
+const ErrorHTTP = errors.ErrorHTTP;
+const server = require('express')();
 
 // use ErrorHTTP to associate a status code and message to an Error object
-server.get('/error', function(req, res, next) {
-  return next(new ErrorHTTP('Error for the sake of errors', 400);
+server.get('/error', (req, res, next) => {
+  return next(new ErrorHTTP('Error for the sake of errors', 400));
 });
 
 // put these after other routes and uses have been defined
 server.use(errors.showError);
 server.use(errors.notFound);
-
 ```
 
 #### test:
--- index.js
+++ index.js
@@ -1,73 +1,73 @@
-var logger = require('fastlog')();
-var http = require('http');
-var util = require('util');
+'use strict';
 
-module.exports = {};
-module.exports.showError = showError;
-module.exports.notFound = notFound;
-module.exports.fastErrorHTTP = fastErrorHTTP;
-module.exports.ErrorHTTP = ErrorHTTP;
+const logger = require('fastlog')();
+const http = require('http');
+const util = require('util');
 
-function showError(err, req, res, next) {
-    err.status = err.status || 500;
+class ErrorHTTP extends Error {
+  constructor(message, status = 500) {
+    super();
 
-    // Output unexpected errors to console but hide them from public eyes.
-    if (err.status >= 500) {
-        err.url = req.url;
-        err.method = req.method;
-        logger.error(err);
-        err.message = 'Internal Server Error';
-    }
-
-    var data = { message:err.message };
-
-    // For non-500 ErrorHTTP objects send over any additional keys.
-    // This error is one that we crafted ourselves deliberately for
-    // public consumption.
-    if (err instanceof ErrorHTTP && err.status < 500) {
-        for (var k in err) {
-            if (k === 'status') continue;
-            data[k] = err[k];
-        }
-    }
-
-    res.status(err.status).jsonp(data);
-}
-
-function notFound(req, res, next) {
-    next(new ErrorHTTP(404));
-}
-
-function fastErrorHTTP(code, status) {
-    if (typeof code !== 'string' && typeof code !== 'number') {
-        throw new Error('code is required to be a string or number');
-    }
-
-    function FastErrorHTTP() {
-        var message = util.format.apply(null, arguments);
-        this.code = code;
-        ErrorHTTP.call(this, message, status);
-    }
-
-    FastErrorHTTP.prototype = Object.create(ErrorHTTP.prototype);
-
-    return FastErrorHTTP;
-}
-
-function ErrorHTTP(message, status) {
     if (typeof message === 'number') {
-        status = message;
-        message = null;
+      status = message;
+      message = null;
     }
-    status = status || 500;
+
     if (!message) {
-        message = http.STATUS_CODES[status];
+      message = http.STATUS_CODES[status];
     }
 
     Error.call(this, message);
-    Error.captureStackTrace(this, arguments.callee);
+    Error.captureStackTrace(this, ErrorHTTP);
     this.message = message;
     this.status = status;
+  }
 }
 
-ErrorHTTP.prototype = Object.create(Error.prototype);
+function fastErrorHTTP(code, status) {
+  if (typeof code !== 'string' && typeof code !== 'number') {
+    throw new Error('code is required to be a string or number');
+  }
+
+  class FastErrorHTTP extends ErrorHTTP {
+    constructor(...params) {
+      const message = util.format(...params);
+      super(message, status);
+      this.code = code;
+    }
+  }
+
+  return FastErrorHTTP;
+}
+
+function showError(err, req, res) {
+  err.status = err.status || 500;
+
+  // Output unexpected errors to console but hide them from public eyes.
+  if (err.status >= 500) {
+    err.url = req.url;
+    err.method = req.method;
+    logger.error(err);
+    err.message = 'Internal Server Error';
+  }
+
+  const data = { message:err.message };
+
+  // For non-500 ErrorHTTP objects send over any additional keys.
+  // This error is one that we crafted ourselves deliberately for
+  // public consumption.
+  if (err instanceof ErrorHTTP && err.status < 500) {
+    for (const k in err) {
+      if (k === 'status') continue;
+      data[k] = err[k];
+    }
+  }
+
+  res.status(err.status).jsonp(data);
+}
+
+function notFound(req, res, next) {
+  next(new ErrorHTTP(404));
+}
+
+module.exports = { showError, notFound, fastErrorHTTP, ErrorHTTP };
