a001003bf37f4a27d055732fc716d90d1506d964
zyatmapbox <zhongyuan.jin@mapbox.com>
2025-02-27 13:57:46 +0200

    fix logger type

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import type { Request, Response, NextFunction } from 'express';
 import { ErrorHTTP } from './error';
 
 export interface ShowErrorOptions {
-  logger?(): void,
+  logger?(...args: unknown[]): void,
 }
 
 /**
